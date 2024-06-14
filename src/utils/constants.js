31747381b6ef6ae16675f8cde0dcbd6e1ed6a772
Divyaa268 <117614772+Divyaa268@users.noreply.github.com>
2024-06-13 21:43:39 -0400

    Added a constants file containing all image urls

---
+++ src/utils/constants.js
@@ -0,0 +1,8 @@
+
+export const data = ['https://people.com/thmb/STgxkFpSY0SX64-tSbpFWH-m6zs=/4000x0/filters:no_upscale():max_bytes(150000):strip_icc():focal(749x164:751x166)/Taylor-Swift-life-101123-tout-b99b188465254ec0a8eb50fa653b51dc.jpg',
+                    'https://people.com/thmb/9yZKtGbW4OcFrKEvYvy0cECFQyo=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc():focal(759x132:761x134)/taylor-swift-rain-eras-tour-052223-3-2d2c17b9468a438c9377f5bde3f9b6ac.jpg',
+                    'https://people.com/thmb/u976_oxDLiq2wo_4Itosza0b8ds=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc():focal(959x225:961x227)/taylor-swift-031823-01-2000-b2ecd14a11914178ac66f7ef69f3a053.jpg',
+                    'https://media.newyorker.com/photos/661d76919c67930184399493/master/w_1600%2Cc_limit/2PMT3JA%2520resized.jpg',
+                    'https://www.musikexpress.de/wp-content/uploads/2024/04/taylor-swift-scaled.jpg'
+
+]
