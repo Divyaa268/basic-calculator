c9579c08c2ab89017abed7a5bcbe181d1a4154e9
Divyaa268 <117614772+Divyaa268@users.noreply.github.com>
2024-06-17 01:00:03 -0400

    built shimmer ui

---
+++ src/ShimmerUI.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const ShimmerUI = () => {
+  return (
+    <div className='grid grid-cols-3'>
+        <div className='p-16 bg-cyan-400 m-5'></div>
+        <div className='p-16 bg-cyan-400 m-5'></div>
+        <div className='p-16 bg-cyan-400 m-5'></div>
+
+        <div>
+            <h1>Loading congtent.....</h1>
+        </div>
+      
+    </div>
+  )
+}
+
+export default ShimmerUI
