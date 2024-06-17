dd66ed9debd1feb87ae61345334abfa93ce78791
Divyaa268 <117614772+Divyaa268@users.noreply.github.com>
2024-06-17 16:40:02 -0400

    added constant comments data

---
+++ src/utils/commentsData.js
@@ -0,0 +1,45 @@
+
+const commentsData = [
+    {
+        name: "Divya",
+        text: "Such an amazing video",
+        replies: [
+
+        ]
+    },
+    {
+        name: "Tanya",
+        text: "wowwww",
+        replies: [
+            {
+            name: "Seema",
+            text: "Cool",
+        replies: [ 
+            {
+            name: "Ajai",
+        text: "Nice video😊",
+        replies: [
+            
+        ]
+            
+            }]
+            
+    }]
+    },
+    {
+    name: "Soham",
+        text: "Cuteee🌻",
+        replies: [
+            {
+                name: "Siddharth",
+                    text: "woahhhh! 🚀",
+                    replies: [
+                        
+                    ]
+                }
+            
+        ]
+    }
+]
+
+export default commentsData;
