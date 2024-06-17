c6044ec6e07e650cb4ca40ef74f8cc48912f8534
Divyaa268 <117614772+Divyaa268@users.noreply.github.com>
2024-06-17 16:38:20 -0400

    built COMMENTS container  COMPONENT

---
+++ src/CommentContainer.js
@@ -0,0 +1,29 @@
+import CommentsList from './CommentsList';
+import commentsData from './utils/commentsData';
+
+const CommentsContainer = () => {
+    return (
+      <div className='m-5 p-2'>
+          <h1 className='text-2xl font-bold'>
+             Comments
+             </h1>
+             {/* <Comments data={commentsData[0]}/> */}
+             <CommentsList comments={commentsData} />
+  
+        </div>
+    )
+  }
+  
+  export default CommentsContainer;
+
+//   CommentsContainer
+//   └── CommentsList (commentsData)
+//       ├── Comment 1 (no replies)
+//       ├── Comment 2
+//       │   ├── Reply 1 (no replies)
+//       │   └── Reply 2
+//       │       └── Reply 2.1
+//       │           └── Reply 2.1.1
+//       │               └── Reply 2.1.1.1
+//       │                   └── Reply 2.1.1.1.1 (no replies)
+//       └── Comment 3 (no replies)
