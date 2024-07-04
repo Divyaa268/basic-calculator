654ed599d327fea6906cd614256d97e06f0a31ac
Divyaa268 <117614772+Divyaa268@users.noreply.github.com>
2024-07-03 22:17:24 -0400

    Added ToDoList component

---
+++ src/FECoding/ToDoList.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import { useState } from 'react'
+
+const ToDoList = () => {
+
+    // array of tasks
+    const [tasks, setTasks] = useState([]);
+
+    // single task
+    const [newTask, setNewTask] = useState("");
+
+    function handleInputChange(eveny) {
+
+    }
+
+    function addTask()
+    {
+
+    }
+
+    function deleteTask()
+    {
+
+    }
+
+    function moveTaskUp()
+    {
+
+    }
+
+    function moveTaskDown()
+    {
+        
+    }
+
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default ToDoList
