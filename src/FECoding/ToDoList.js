eceee6454b8153258ac7c3fda8cb54fd336245bf
Divyaa268 <117614772+Divyaa268@users.noreply.github.com>
2024-07-03 22:41:27 -0400

    Added ToDoList functions and jsx

--- src/FECoding/ToDoList.js
+++ src/FECoding/ToDoList.js
@@ -4,12 +4,15 @@ import { useState } from 'react'
 const ToDoList = () => {
 
     // array of tasks
-    const [tasks, setTasks] = useState([]);
+    const [tasks, setTasks] = useState(["LeetCode DSA", "LeetCode JS", "FE Coding"]);
 
     // single task
     const [newTask, setNewTask] = useState("");
 
-    function handleInputChange(eveny) {
+    // We use this function to see the text we type in input field
+    // otherwise t won't be visible when we type it 
+    function handleInputChange(event) {
+        setNewTask(event.target.value)
 
     }
 
@@ -30,12 +33,42 @@ const ToDoList = () => {
 
     function moveTaskDown()
     {
-        
+
     }
 
   return (
-    <div>
+    <div className='to-do-list'>
+        <h1>To Do List</h1>
+
+        <input
+        type="text"
+        placeholder='Enter your task...'
+        onChange={handleInputChange}
+        value={newTask} />
+
+        <button 
+        className='add-task'
+        onClick={addTask}> Add </button>
       
+      <ol>
+        {tasks.map((task, index) => 
+        <li key={index}>
+            <span className='text'>{task}</span>
+            <button className='delete-button' onClick={() => deleteTask(index)}>
+                Delete
+            </button>
+
+            <button className='up-button' onClick={() => moveTaskUp(index)}>
+                ⬆️
+            </button>
+
+            <button className='down-button' onClick={() => moveTaskDown(index)}>
+                ⬇️
+            </button>
+
+        </li>
+        )}
+      </ol>
     </div>
   )
 }
