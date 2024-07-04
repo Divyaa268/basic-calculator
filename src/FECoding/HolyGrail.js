c67bf763036fadb3324ff0599f5200fdd6ebb77f
Divyaa268 <117614772+Divyaa268@users.noreply.github.com>
2024-07-03 21:32:58 -0400

    Added Holygrail  component

---
+++ src/FECoding/HolyGrail.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const HolyGrail = () => {
+  return (
+    <div className='container'>
+        <header></header>
+        <nav></nav>
+        <main></main>
+        <aside></aside>
+        <footer></footer>
+    </div>
+  )
+}
+
+export default HolyGrail
