c5df96445de9e54b62cdb485421f3907266b5e50
Divyaa268 <117614772+Divyaa268@users.noreply.github.com>
2024-07-04 22:04:50 -0400

    Updated component

--- src/FECoding/TrafficLight.js
+++ src/FECoding/TrafficLight.js
@@ -1,12 +1,40 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import '../traffic.css'
 
-const TrafficLight = () => {
+const TrafficLight = ({trafficStates}) => {
+
+    const [currentColor, setCurrentColor] = useState("green");
+
+    useEffect(() => {
+        // destructing the other properties from trafficStates
+        const {duration, next} = trafficStates[currentColor];
+
+        // after this change the  current color to next color based on the duration
+        const timerId = setTimeout(() => {
+            setCurrentColor(next);
+        }, duration);
+
+        return () => {
+            clearTimeout(timerId);
+        }
+
+    }, [currentColor])
+
   return (
     <div className='traffic-light-container'>
 
-        {[1,2,3].map(() => 
-        <div className='traffic-light'>
+{/* this returns an array containing all keys of traffic states which in this case is
+red, yellow, green */}
+        {Object.keys(trafficStates).map((color) => 
+        <div className='traffic-light' 
+        // to dynamically change the color
+        style={{
+            backgroundColor: 
+            color === currentColor && trafficStates[color].backgroundColor
+        }}>
+            {/* now switch the colors dynamically after a certain duration/intervals
+            we will use useEffect for this which will have the currentColor as the dependency
+            so that whenever this currentColor changes, useEffect will be called */}
             </div>)}
       
     </div>
